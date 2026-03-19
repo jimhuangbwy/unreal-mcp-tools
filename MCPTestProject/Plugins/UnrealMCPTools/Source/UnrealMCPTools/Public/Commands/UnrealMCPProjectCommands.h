78f9b72c2689153e2674cc8339710962fa2320a4
jimmy <jimmy@zoulstudio.com>
2026-03-19 15:50:09 +0800

    Init commit

---
+++ MCPTestProject/Plugins/UnrealMCPTools/Source/UnrealMCPTools/Public/Commands/UnrealMCPProjectCommands.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Json.h"
+
+/**
+ * Handler class for Project-wide MCP commands
+ */
+class UNREALMCPTOOLS_API FUnrealMCPProjectCommands
+{
+public:
+    FUnrealMCPProjectCommands();
+
+    // Handle project commands
+    TSharedPtr<FJsonObject> HandleCommand(const FString& CommandType, const TSharedPtr<FJsonObject>& Params);
+
+private:
+    // Specific project command handlers
+    TSharedPtr<FJsonObject> HandleCreateInputMapping(const TSharedPtr<FJsonObject>& Params);
+}; 
